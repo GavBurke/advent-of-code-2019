ca03ccc8027741f04b4d50421a03e0da5f47bae6
Gavin Burke <gavburke85@gmail.com>
2019-12-10 16:35:12 +0000

    Create Puzzle2WithoutFunction.py

---
+++ Day2/Puzzle2WithoutFunction.py
@@ -0,0 +1,26 @@
+import numpy
+data = (1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,5,23,1,23,9,27,2,27,6,31,1,31,6,35,2,35,9,39,1,6,39,43,2,10,43,47,1,47,9,51,1,51,6,55,1,55,6,59,2,59,10,63,1,6,63,67,2,6,67,71,1,71,5,75,2,13,75,79,1,10,79,83,1,5,83,87,2,87,10,91,1,5,91,95,2,95,6,99,1,99,6,103,2,103,6,107,2,107,9,111,1,111,5,115,1,115,6,119,2,6,119,123,1,5,123,127,1,127,13,131,1,2,131,135,1,135,10,0,99,2,14,0,0)
+
+
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        array = numpy.asarray(data)
+        array[1] = noun
+        array[2] = verb
+        current  = 0
+        while current <= len(array):
+            if array[current] == 1:
+                array[array[current+3]] = array[array[current+1]] + array[array[current+2]]
+            elif array[current] == 2:
+                array[array[current+3]] = array[array[current+1]] * array[array[current+2]]
+            elif array[current] == 99:
+                #print("end")
+                break
+            else:
+                #print("error")
+                break
+            current+=4
+        if array[0] == 19690720:
+            print(100 * noun + verb)
+
+
