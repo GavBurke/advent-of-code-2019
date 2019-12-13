c8705dba81cff8fffec939e2b35dcf6775c190c0
Gavin Burke <gavburke85@gmail.com>
2019-12-13 10:54:53 +0000

    Create Puzzle2.py

---
+++ Day4/Puzzle2.py
@@ -0,0 +1,28 @@
+puzzleMin     = 254032
+puzzleMax     = 789860
+puzzleCurrent = puzzleMin
+puzzleCount   = 0
+
+while puzzleCurrent <= puzzleMax:
+    isRepeated   = False
+    isDecreasing = False
+    puzzleCurrentArray = list(map(int, str(puzzleCurrent)))
+    i = 0
+    while i < 5:
+        if puzzleCurrentArray[i] == puzzleCurrentArray[i+1]:
+            if isRepeated == True:
+                isRepeated = False
+            else:
+                isRepeated = True
+        i+=1
+    i = 0
+    while i < 5:
+        if puzzleCurrentArray[i] > puzzleCurrentArray[i+1]:
+            isDecreasing = True
+        i+=1
+    puzzleCurrent+=1
+    if isRepeated == True and isDecreasing == False:
+        puzzleCount+=1
+
+print(puzzleCount)
+
